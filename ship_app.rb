5d9fad0b3b73af1f14289e6734266d74b0eb117f
Washington Luiz <huoxito@gmail.com>
2014-02-21 17:11:57 -0300

    Add three more hook endpoints

--- ship_app.rb
+++ ship_app.rb
@@ -4,15 +4,50 @@ require 'json'
 class ShipApp < Sinatra::Base
   post "/get_shipments" do
     content_type :json
+    request_id = params[:request_id]
 
-    shipments = Service.new.shipments_since("2014-02-19T18:23:48Z")
-    { request_id: "4545423523452345", shipments: shipments }.to_json
+    shipments = Service.new(payload).shipments_since
+    { request_id: request_id, shipments: shipments }.to_json
+  end
+
+  post "/add_shipment" do
+    content_type :json
+    request_id = params[:request_id]
+    payload = params[:shipment]
+
+    shipment = Service.new(payload).create
+    { request_id: request_id, summary: "Shipment #{shipment.to_s} was added" }.to_json
+  end
+
+  post "/update_shipment" do
+    content_type :json
+    request_id = params[:request_id]
+    payload = params[:shipment]
+    shipment = Service.new(payload).update
+
+    { request_id: request_id, summary: "Shipment #{shipment.to_s} was updated" }.to_json
+  end
+  
+  # Custom webhook
+  post "/cancel_shipment" do
+    content_type :json
+    request_id = params[:request_id]
+    payload = params[:shipment]
+    shipment = Service.new(payload).cancel
+
+    { request_id: request_id, summary: "Shipment #{shipment.to_s} was canceled" }.to_json
   end
 end
 
-# Communicate with your Shipment service here
 class Service
-  def shipments_since(timestamp)
+  attr_reader :payload
+
+  def initialize(payload = {})
+    @payload = payload
+  end
+
+  # Search for shipments after a given timestamp, e.g. payload[:created_after]
+  def shipments_since
     [
       {
         "id" => "12836",
@@ -20,5 +55,23 @@ class Service
         "tracking" => "12345678"
       }
     ]
+  end
+
+  # Talk to you shipment api making, e.g.
+  #   FedEx.create_shipment payload
+  def create
+    "022014"
+  end
+
+  # Talk to you shipment api making, e.g.
+  #   FedEx.create_shipment payload
+  def update
+    "022014"
+  end
+
+  # Talk to you shipment api making, e.g.
+  #   FedEx.cancel_shipment payload
+  def cancel
+    "022014"
   end
 end
